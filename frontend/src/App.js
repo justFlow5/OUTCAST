78eb444b60a2b000051085536a72446852d609e7
Michał Skrzypczyński <justflow555@gmail.com>
2020-10-26 14:30:34 +0100

    Add products description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 import './App.css';
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
             <Header />
             <main className="py-3">
                 <Container>
-                    <h1>Hrllo</h1>
+                    <HomeScreen />
                 </Container>
             </main>
             <Footer />
