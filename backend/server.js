62e3832dfd3a986c786e6fb6ed42d8b932bde963
Michał Skrzypczyński <justflow555@gmail.com>
2020-10-26 16:21:05 +0100

    Add get requests for fetching products

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API IS RUNNING');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/product/:id', (req, res) => {
+    const product = products.find((product) => product._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(5000, console.log('app is running on port 5000'));
